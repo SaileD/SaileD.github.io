dfa34c12a9220651d7227673af0e286fd93e08db
SaileD <slymple@gmail.com>
2017-06-14 23:12:11 -0700

    Some background changes

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-$(document).ready(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+$("button").click(function () {
+    $("#content").hide();
+    $("#yt")[0].src += "?autoplay=1";
+    setTimeout(function(){ $("#yt").show(); }, 500);
 });
